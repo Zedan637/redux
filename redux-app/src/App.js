2094e4f51854b954205c24cf905dddc79dc2c46f
zedan <you@example.com>
2023-02-18 18:16:03 +0530

    another commit

--- redux-app/src/App.js
+++ redux-app/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
 function App() {
+  console.log("Testing code in background...")
   return (
     <div>
       <h1>Hello World!!</h1>
-      console.log("Hello World...")
       <h1>Addtional line of code added...</h1>
+      <h1>Another line added...</h1>
     </div>
   );
 }
